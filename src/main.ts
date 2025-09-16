c88b58a3ace84c9c3c772d94b29be5cb7d22d7e6
Tanke6003 <Ruben.Farias.1999@Outlook.es>
2025-09-15 19:58:37 -0600

    and envs plugin

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 // src/main.ts
 import { Server } from "./core/server.js";
+import { DotenvPlugin } from "./infrastructure/plugins/dotenv.plugin.js";
 
 
 
 
 (async()=>{
-    const server = new Server(3000);
+    const envs = new DotenvPlugin();
+    const portEnv = envs.getEnv("PORT");
+    const port = portEnv === undefined || portEnv === null || portEnv === "" ? 3000 : Number(portEnv);
+    const server = new Server(port);
     await server.run();
 })();
