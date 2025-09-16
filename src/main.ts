f7ba76f6e07422a750ab6b3683cbb74cf812fae3
Tanke6003 <Ruben.Farias.1999@Outlook.es>
2025-09-16 10:41:46 -0600

    app

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { Server } from "./core/server";
 import { DotenvPlugin } from "./infrastructure/plugins/dotenv.plugin";
 
 
-
-
-(async()=>{
-    const envs = new DotenvPlugin();
+ const envs = new DotenvPlugin();
     const portEnv = envs.getEnv("PORT");
     const port = portEnv === undefined || portEnv === null || portEnv === "" ? 3000 : Number(portEnv);
     const server = new Server(port);
+
+(async()=>{
+   
     await server.run();
 })();
+
+export default server.app;
