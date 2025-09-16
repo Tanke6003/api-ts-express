96122a16d4761ac9a667bb3a24f5117763bac320
Tanke6003 <Ruben.Farias.1999@Outlook.es>
2025-09-16 10:04:24 -0600

    [fix] replace vercel js

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,22 @@
 //     await server.run();
 // })();
 // src/main.ts
+// import { Server } from "./core/server";
+// import { DotenvPlugin } from "./infrastructure/plugins/dotenv.plugin";
+
+// const envs = new DotenvPlugin();
+// const portEnv = envs.getEnv("PORT");
+// const port = portEnv === undefined || portEnv === null || portEnv === "" ? 3000 : Number(portEnv);
+// const server = new Server(port);
+
+// // Exportar app para Vercel
+// export default server.app;
+
+// // Correr solo localmente
+// (async () => {
+//   await server.run();
+// })();
+// src/main.ts
 import { Server } from "./core/server";
 import { DotenvPlugin } from "./infrastructure/plugins/dotenv.plugin";
 
@@ -21,10 +37,12 @@ const portEnv = envs.getEnv("PORT");
 const port = portEnv === undefined || portEnv === null || portEnv === "" ? 3000 : Number(portEnv);
 const server = new Server(port);
 
-// Exportar app para Vercel
+// Exportamos la app de Express para Vercel
 export default server.app;
 
-// Correr solo localmente
-(async () => {
-  await server.run();
-})();
+// Solo ejecutamos .listen si corremos localmente
+if (process.env.VERCEL !== "1") {
+  (async () => {
+    await server.run();
+  })();
+}
